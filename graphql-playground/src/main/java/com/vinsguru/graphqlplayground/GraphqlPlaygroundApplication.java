edf4ecd4a8cb2a171f2cc08b18a09246ff880d08
vinsguru <vino.infy@gmail.com>
2023-11-26 12:51:35 -0700

    updated for latest spring boot version 3.2.0 + java21

--- graphql-playground/src/main/java/com/vinsguru/graphqlplayground/GraphqlPlaygroundApplication.java
+++ graphql-playground/src/main/java/com/vinsguru/graphqlplayground/GraphqlPlaygroundApplication.java
@@ -1,10 +1,13 @@
 package com.vinsguru.graphqlplayground;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.vinsguru.graphqlplayground.${lec}")
+//, exclude = R2dbcAutoConfiguration.class
 @EnableR2dbcRepositories(basePackages = "com.vinsguru.graphqlplayground.${lec}")
 public class GraphqlPlaygroundApplication {
 
